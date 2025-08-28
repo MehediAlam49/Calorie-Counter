ac7f4cb7ee5329e3de1831d54f4a3ae9950cb283
MehediAlam49 <mehedialam806@gmail.com>
2025-08-28 19:52:08 +0600

    Add templates for user authentication and dashboard functionality

--- calorie_counter/calorie_counter_app/views.py
+++ calorie_counter/calorie_counter_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+def register_view(request):
+    
+    return render(request, 'register.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def logout_view(request):
+    return redirect('login_view')
+
+def dashboard(request):
+    return render(request, 'home.html')
