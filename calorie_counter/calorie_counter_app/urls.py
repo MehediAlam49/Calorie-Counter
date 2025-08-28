07777c0285169ce0be0e329588cebcf37be986a1
MehediAlam49 <mehedialam806@gmail.com>
2025-08-28 20:19:42 +0600

    Implement user authentication views and URL routing; update templates for consistency

---
+++ calorie_counter/calorie_counter_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', register_view, name='register_view'),
+    path('', login_view, name='login_view'),
+    path('logout/', logout_view, name='logout_view'),
+    path('home/', home, name='home'),
+    path('profile/', profile, name='profile'),
+]
--- calorie_counter/calorie_counter_app/views.py
+++ calorie_counter/calorie_counter_app/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
+from .models import *
 
 # Create your views here.
 def register_view(request):
-    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        user_exists = CustomUserModel.objects.filter(username = username).exists()
+        if user_exists:
+            return redirect('register_view')
+        else:
+            if password == confirm_password:
+                user = CustomUserModel.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                )
+
+                if user:
+                    UserProfileModel.objects.create(
+                        user=user,
+                    )
+                    return redirect('login_view')
     return render(request, 'register.html')
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('dashboard')
     return render(request, 'login.html')
 
 def logout_view(request):
+    logout(request)
     return redirect('login_view')
 
-def dashboard(request):
+def home(request):
     return render(request, 'home.html')
+def profile(request):
+    return render(request, 'profile.html')
